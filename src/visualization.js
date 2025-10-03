2491a4163037ca4ada23f5c2aec99e1c14ce01b4
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-03 19:21:49 +0300

    feat: initial commit for src/visualization.js

---
+++ src/visualization.js
@@ -0,0 +1,8 @@
+// Modul zur Visualisierung von Commits
+
+function drawCommitGraph(commits) {
+  // Funktion, um einen Commit-Verlauf zu zeichnen
+  // TODO: Implementierung hinzufügen
+}
+
+module.exports = { drawCommitGraph };
