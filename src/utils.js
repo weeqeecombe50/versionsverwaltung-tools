c8e267c2b637e79d8cb0af44568bc403401519bd
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-03 19:21:47 +0300

    feat: initial commit for src/utils.js

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+// Hilfsfunktionen
+
+function isGitRepo(path) {
+  // Funktion zur Überprüfung, ob der angegebene Pfad ein Git-Repository ist
+  // TODO: Implementierung hinzufügen
+}
+
+function fetchCommits() {
+  // Funktion zum Abrufen von Commits
+  // TODO: Implementierung hinzufügen
+}
+
+module.exports = { isGitRepo, fetchCommits };
